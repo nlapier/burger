0dc6b8e03ffb165030f99b20c6c88f8e9bc6f69d
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-08-10 20:46:35 -0400

    added public directory and boilderplate setup in server.js

--- server.js
+++ server.js
@@ -1,4 +1,34 @@
-var express = require("express");
+//Dependencies
 var override = require ("method-override");
-var bp = require ("body-parser");
+var hB = require('express-handlebars');
+var bP = require ("body-parser");
 var mysql = require("mysql");
+var express = require("express");
+var app = express();
+
+
+// Connection to mysql:
+module.exports = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	port: 3306,
+	database: 'burger_db'
+});
+
+//POST Override
+app.use(override("_method"));
+
+//Set up Handlebars
+app.engine("handlebars", hB({
+	defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//Get static content from "public" directory
+app.use(express.static(process.cwd() + "/public"));
+
+//Set up body parser
+app.use(bP.urlencoded({
+	extended: false
+}));
